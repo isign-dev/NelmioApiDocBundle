9994b79927efc01f3d0ada59877f90e97ae14e86
Andrejus Komarovas <andrejus.komarovas@signicat.com>
2023-07-15 13:10:09 +0300

    Use DI in ApiDocController

--- Controller/ApiDocController.php
+++ Controller/ApiDocController.php
@@ -11,8 +11,11 @@
 
 namespace Nelmio\ApiDocBundle\Controller;
 
+use Nelmio\ApiDocBundle\Extractor\ApiDocExtractor;
+use Nelmio\ApiDocBundle\Formatter\HtmlFormatter;
 use Nelmio\ApiDocBundle\Formatter\RequestAwareSwaggerFormatter;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Nelmio\ApiDocBundle\Formatter\SwaggerFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,19 +23,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApiDocController extends AbstractController
 {
+    public function __construct(
+        private ApiDocExtractor $apiDocExtractor,
+        private HtmlFormatter $htmlFormatter,
+        private SwaggerFormatter $swaggerFormatter,
+    ) {
+    }
+
     public function indexAction($view = ApiDoc::DEFAULT_VIEW)
     {
-        $extractedDoc = $this->get('nelmio_api_doc.extractor.api_doc_extractor')->all($view);
-        $htmlContent  = $this->get('nelmio_api_doc.formatter.html_formatter')->format($extractedDoc);
+        $extractedDoc = $this->apiDocExtractor->all($view);
+        $htmlContent  = $this->htmlFormatter->format($extractedDoc);
 
         return new Response($htmlContent, 200, array('Content-Type' => 'text/html'));
     }
 
     public function swaggerAction(Request $request, $resource = null)
     {
-
-        $docs = $this->get('nelmio_api_doc.extractor.api_doc_extractor')->all();
-        $formatter = new RequestAwareSwaggerFormatter($request, $this->get('nelmio_api_doc.formatter.swagger_formatter'));
+        $docs = $this->apiDocExtractor->all();
+        $formatter = new RequestAwareSwaggerFormatter($request, $this->swaggerFormatter);
 
         $spec = $formatter->format($docs, $resource ? '/' . $resource : null);
 
